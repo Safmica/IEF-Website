1b747a20bf04729b55ea045f941c07cc62dfac08
Tadonoki <khailanilupi2005@gmail.com>
2024-10-08 10:55:55 +0700

    Home and Timeline

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\adminController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get('/login', function () {
